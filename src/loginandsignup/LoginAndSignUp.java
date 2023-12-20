a3b0ec7bceb64c76268f348f74256917ffac3df5
dushimec <dushimec515@gmail.com>
2023-12-20 13:58:23 +0000

    first commit

---
+++ src/loginandsignup/LoginAndSignUp.java
@@ -0,0 +1,15 @@
+
+package loginandsignup;
+
+public class LoginAndSignUp {
+
+
+    public static void main(String[] args) {
+
+        index LoginFrame = new index();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
